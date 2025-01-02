8bc6dfed43f8eda58df7741451d2a91caeede993
Amresh SHARMA <amresh.sharma.ext@bureauveritas.com>
2025-01-02 17:00:08 +0530

    db sync added

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 export default {
   env: process.env.NODE_ENV,
+  syncDatabase: process.env.SYNC_DATABASE || false,
+  syncDatabaseForce: process.env.SYNC_DATABASE_FORCE || false,
   databaseURL: process.env.DATABASE_URL,
   databaseSSLRequire: process.env.DATABASE_SSL_REQUIRE === 'true',
   databaseSSLRejectUnauthorized:
